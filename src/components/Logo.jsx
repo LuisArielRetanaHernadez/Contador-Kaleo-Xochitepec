e9321a866235ea3172426886d527a2e9b5b7576c
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2025-05-09 22:34:27 -0600

    creando el componente Logo sin aun la imagen del logo destinaod a colocar

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="/images/logo.png" alt="logo" />
+      <div>
+        <p>Kaleo</p>
+      </div>
+    </div>
+  )
+
+}
+
+export default Logo
